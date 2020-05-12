eef1fe10c5f2cc5b495f52e481058613f8f865f8
Divyansh <57042143+Divyanshwick@users.noreply.github.com>
2020-05-12 14:05:31 +0530

    Update Activity_Selection.cpp

--- Activity_Selection.cpp
+++ Activity_Selection.cpp
@@ -2,21 +2,25 @@
 #include<algorithm>
 using namespace std;
 int main(){
-    int n,i,cnt=1,j=0;
+    int n,i=0,cnt=1,j;
+    
     cin >> n;
+    
     int start[n],finish[n];
+    
     for(i=0;i<n;cin >> start[i++]);
+    
     for(i=0;i<n;cin >> finish[i++]);
+    
     sort(finish,finish+n);
-    for(i=0;i<n-1;i++){
-        if(start[i+1] >= finish[j]){
+    
+    for(j=1;j<n;j++){
+        if(start[j] >= finish[i]){
             cnt++;
-            j=i;;
+            i=j;;
         }
     }
-    cout << cnt;
+    cout << "The max no. of activities done are: " << cnt;
 
-    
-    
-   return 0;
+return 0;
 }
